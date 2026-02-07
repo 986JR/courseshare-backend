c93f2397d211e02981afad3fa7660c4aaa3856dc
JOSHUA RODGERS <joshuaridgers986@gmail.com>
2026-02-07 09:53:57 +0300

    Docker container for Backedn is now working in my machine

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,17 @@
-# Use official OpenJDK image
-FROM eclipse-temurin:21-jdk-alpine
-
-# Set working directory
+FROM eclipse-temurin:21-jdk
 WORKDIR /app
 
-# Copy Maven wrapper and pom.xml first (for caching)
 COPY mvnw .
 COPY .mvn .mvn
 COPY pom.xml .
 
-# Download dependencies
 RUN ./mvnw dependency:go-offline -B
 
-# Copy source code
 COPY src src
-
-# Build the jar
 RUN ./mvnw package -DskipTests
 
-# Expose port (Render uses 8080)
-EXPOSE 8080
+# copy env BEFORE running
+COPY .env .env
 
-# Run the app
+EXPOSE 8080
 ENTRYPOINT ["java","-jar","target/courseshare-0.0.1-SNAPSHOT.jar"]
--- src/main/java/backend/courseshare/CourseshareApplication.java
+++ src/main/java/backend/courseshare/CourseshareApplication.java
@@ -1,6 +1,5 @@
 package backend.courseshare;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,9 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CourseshareApplication {
 
 	public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(CourseshareApplication.class, args);
 
